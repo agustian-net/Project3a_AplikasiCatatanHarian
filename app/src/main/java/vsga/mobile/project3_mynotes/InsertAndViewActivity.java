133875ad5d9df00f5ef4cc8ce31b19d8102803d1
egi-agustian <egiagustianto1994@gmail.com>
2021-08-15 01:02:15 +0700

    Initial Commit dan menambahkan interface

---
+++ app/src/main/java/vsga/mobile/project3_mynotes/InsertAndViewActivity.java
@@ -0,0 +1,8 @@
+package vsga.mobile.project3_mynotes;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InsertAndViewActivity extends AppCompatActivity {
+}
